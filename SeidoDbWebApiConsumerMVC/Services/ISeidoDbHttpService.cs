e355e9db1d05540bd0b52b34369116794bf729a4
Lars Asplund <lars.asplund@hotmail.com>
2022-04-11 11:39:45 +0200

    Model och 44340

--- SeidoDbWebApiConsumerMVC/Services/ISeidoDbHttpService.cs
+++ SeidoDbWebApiConsumerMVC/Services/ISeidoDbHttpService.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using SeidoDbWebApiConsumerMVC.Models;
+using NecklaceModels;
 
 namespace SeidoDbWebApiConsumerMVC.Services
 {
