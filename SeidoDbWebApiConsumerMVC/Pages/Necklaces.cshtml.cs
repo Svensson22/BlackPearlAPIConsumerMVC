bfca2b9cbc40cc7931b6c0d91c2654070a851e16
GabrielFredlund <75130085+GabrielFredlund@users.noreply.github.com>
2022-04-12 11:02:52 +0200

    models & pages

---
+++ SeidoDbWebApiConsumerMVC/Pages/Necklaces.cshtml.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using NecklaceModels;
+using SeidoDbWebApiConsumerMVC.Services;
+
+namespace SeidoDbWebApiConsumerMVC.Pages
+{
+    public class NecklacesModel : PageModel
+    {
+        ISeidoDbHttpService _httpService;
+
+
+        public IEnumerable<Necklace> Necklaces { get; private set; }
+
+
+        [BindProperty]
+        public Necklace NewNecklace { get; set; }
+
+        public async Task OnGet()
+        {
+            Necklaces = await _httpService.GetNecklacesAsync();
+            Necklaces = Necklaces.Take(10);
+        }
+
+
+        public async Task<IActionResult> OnPostDelete(string id)
+        {
+
+            int.TryParse(id, out int NeckID);
+            await _httpService.DeleteNecklaceAsync(NeckID);
+
+            return RedirectToPage("/necklaces");
+        }
+
+
+
+
+       public NecklacesModel(ISeidoDbHttpService service)
+        {
+            this._httpService = service;
+        }
+    }
+}
